6c361e6b18b17527c1865e80cd22e07edb4714fe
faishal2727 <muhammadfaishalrizal27@gmail.com>
2024-02-19 01:37:20 +0700

    update testing

---
+++ android/app/src/main/kotlin/com/example/film_ku_submissi_expert/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.film_ku_submissi_expert
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/submissi_flutter_expert/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.submissi_flutter_expert
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
